10d47ba556f3e7376429f2de924d18b791d72e72
Darko Krizic <darko@krizic.net>
2021-11-11 13:36:36 +0200

    First version for dkrizic

---
+++ src/dkrizic/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type server struct {}
+
+func main() {
+  s := server{}
+
+  log.Fatal(http.ListenAndServe(":8080", &s))
+}
+
+func (s server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(200)
+	w.Write([]byte(`{ "ok" : true , "status": "ok", "instance": "dkrizic"}`))
+}
