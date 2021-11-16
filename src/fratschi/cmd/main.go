1f7d06296cdb434830e29525a1a00c16b576c6f5
Frank Ratschinski <fratschin@gmail.com>
2021-11-16 12:21:39 +0100

    next iteration

--- src/fratschi/cmd/main.go
+++ src/fratschi/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
 )
@@ -11,11 +12,16 @@ func main() {
 	s := server{}
 
 	log.Printf("Starting server")
+
+	go func() {
+		log.Fatal(http.ListenAndServe(":8083",promhttp.Handler()))
+	}()
+
 	log.Fatal(http.ListenAndServe(":8080", &s))
 }
 
 func (s server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
-	w.Write([]byte(`{ "ok" : true , "status": "ok", "instance": "fratschi", "version": "4"}`))
+	w.Write([]byte(`{ "ok" : true , "status": "ok", "instance": "fratschi", "version": "5"}`))
 	log.Printf("One request from %s", r.RemoteAddr)
 }
