75ce35d0ee13b9edd9824cd69fba5a2f879c7385
Ronald Müller <ronald.mueller@prodyna.com>
2021-11-16 17:21:23 +0100

    added instance for xlerate

---
+++ src/xlerate/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type server struct{}
+
+func main() {
+	s := server{}
+
+	log.Printf("Starting server")
+	log.Fatal(http.ListenAndServe(":8080", &s))
+}
+
+func (s server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte(`{ "ok" : true , "status": "ok", "instance": "xlerate", "version": "4"}`))
+	log.Printf("One request from %s", r.RemoteAddr)
+}
