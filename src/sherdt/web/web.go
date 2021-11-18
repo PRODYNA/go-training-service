0106f99039a107cc3b9581c9eb271b4998c462f5
Sergej Herdt <sergej.herdt@prodyna.com>
2021-11-18 21:56:02 +0100

    added static html to web path /

--- src/sherdt/web/web.go
+++ src/sherdt/web/web.go
@@ -12,9 +12,47 @@ func HandleWebPort(router *mux.Router) {
 		Path("/status").
 		HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 			writer.WriteHeader(200)
-			_, err := writer.Write([]byte(`{ "status": "ok"}`))
+			_, err := writer.Write([]byte(`{ "status": "up"}`))
+			if err != nil {
+				return
+			}
+		})
+
+	router.
+		Methods("GET").
+		Path("/").
+		HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			writer.WriteHeader(200)
+			_, err := writer.Write([]byte(html))
 			if err != nil {
 				return
 			}
 		})
 }
+
+var html = `
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Go - Hello World</title>
+		<style>
+			body {
+				background-color: #545454;
+			}
+			h1 {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				height: 90vh;
+				color: white;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>
+		Hello World, written in Go.
+		</h1>
+	</body>
+</html>
+`
